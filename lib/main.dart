6d30fc9248ba44d444707453f3d7d964b0c9f8d2
maggiehorowitz <m_horowitz@coloradocollege.edu>
2020-10-18 17:41:57 -0400

    new transaction separate popup and formatting fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:mBudget/widgets/user_transactions.dart';
-import './widgets/user_transactions.dart';
+import './widgets/tansaction_list.dart';
+import './widgets/new_transaction.dart';
+
 import 'package:intl/intl.dart';
+
+import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,19 +18,76 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   final titleController = TextEditingController();
+
   final amountController = TextEditingController();
+
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'Macbook Pro 16-inch',
+      amount: 2999.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Macbook Pro 13-inch',
+      amount: 1499.99,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap:
+                () {}, //Nothing happens if you click the popup itself, but disappears if you click the background
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+
+            ///avoids that tap on space might tap to background
+          ); //widget you want to show in the modal sheet
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.teal[300],
-          title: Text('mBudget'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+      appBar: AppBar(
+        backgroundColor: Colors.teal[300],
+        title: Text('Can I Afford This?'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Icons.add,
+              ),
+              onPressed: () => _startAddNewTransaction(context))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
@@ -38,8 +98,14 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            UserTransactions()
+            TransactionList(_userTransactions),
           ],
-        ));
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => _startAddNewTransaction(context)),
+    );
   }
 }
--- lib/widgets/tansaction_list.dart
+++ lib/widgets/tansaction_list.dart
@@ -9,56 +9,62 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 2,
+    return Container(
+      height: 520,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return //widget built for each item, so the Card code was moved up here
+              Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
                   ),
-                ),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  '\$ ${tx.amount}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.black,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    ),
                   ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tx.title,
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$ ${transactions[index].amount.toStringAsFixed(2)}',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                      fontSize: 20,
                       color: Colors.black,
                     ),
                   ),
-                  Text(
-                    DateFormat().format(tx.date),
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
                     ),
-                  )
-                ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                    Text(
+                      DateFormat().format(transactions[index].date),
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
+        }, //takes in a context and index
+        itemCount: transactions.length, //required for ListView.builder
+        // children: transactions.map((tx) {}).toList(), ///This was used when the "Return Card() code was after and not in a ListBuilder, it was in a ListView"
+      ),
     );
   }
 }
